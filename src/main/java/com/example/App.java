3d825da26ab2970c85aa80f444f84c299ee98289
Nabeelanaushadkhan <nabeelakhan9869@gmail.com>
2026-04-17 08:47:17 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This Is Nabeela Khan Usn: 1BI24CS412" );
+    }
+}
